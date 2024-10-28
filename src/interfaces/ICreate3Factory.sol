959233c3742131de85443e7272999e2ab288852d
chef-burger <137024020+chefburger@users.noreply.github.com>
2024-10-28 10:29:58 +0800

    fix: make sure fund specified from args match msg.value (#5)

--- src/interfaces/ICreate3Factory.sol
+++ src/interfaces/ICreate3Factory.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 interface ICreate3Factory {
     error NotWhitelisted();
     error CreationCodeHashMismatch();
+    error FundsAmountMismatch();
 
     /**
      * @notice create3 deploy a contract
